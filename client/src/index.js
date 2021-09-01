6109b2a9e606ca50fdbeacb53e4dd179fb5226ce
leus-nick <nickaccreg@gmail.com>
2021-09-01 16:49:31 +0300

    The basic structure of the project. Added React for the client, Express and other necessary dependencies for the server.

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+import './remedy.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
+serviceWorker.unregister();
